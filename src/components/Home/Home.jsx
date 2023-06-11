12e416b31939575ac0b0164a7850e940380d0ac9
Stephin <stephinreji123@gmail.com>
2023-06-11 08:30:03 +0530

    updated color pallete

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,15 +1,15 @@
 import "./home.css";
 
-const IndianLegalActs = [
-  "Indian Penal Code",
-  "Code of Criminal Procedure",
-  "Civil Procedure Code",
-  "Indian Evidence Act",
-  "Hindu Marriage Act",
-  "Indian Divorce Act",
-  "Negotiable Instruments Act",
-  "The Motor Vehicles Act",
-];
+// const IndianLegalActs = [
+//   "Indian Penal Code",
+//   "Code of Criminal Procedure",
+//   "Civil Procedure Code",
+//   "Indian Evidence Act",
+//   "Hindu Marriage Act",
+//   "Indian Divorce Act",
+//   "Negotiable Instruments Act",
+//   "The Motor Vehicles Act",
+// ];
 
 const Home = () => {
   return (
@@ -17,6 +17,9 @@ const Home = () => {
       <div className='home-section-1'>
         <div className='home-section-1-title'>
           <h1>Empower Yourself: Know Your Rights</h1>
+        </div>
+        <div className='home-section-1-tagline'>
+          <p>The Ultimate App for Legal Knowledge and Empowerment!</p>
         </div>
       </div>
       <div className='home-section-2'>
